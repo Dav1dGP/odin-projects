755c067c82b7fb1c010ceb58b3940d4176031f11
DavidGaspar <david.gaspar.alfaz@gmail.com>
2024-06-18 11:57:58 +0200

    write the logic to play a single round in JavaScript

--- rock-paper-scissors/javascript.js
+++ rock-paper-scissors/javascript.js
@@ -1,4 +1,3 @@
-
 let humanScore = 0;
 let computerScore = 0;
 
@@ -8,7 +7,7 @@ function getComputerChoice () {
     return choices[randomChoices];
 }
 
-//console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
 function getHumanChoice(){
     const humanChoice = prompt ("¿Choose one: rock, paper or scissors?");
@@ -17,3 +16,31 @@ function getHumanChoice(){
 
 // console.log(getHumanChoice());
 
+function playRound (humanChoice, computerChoice){
+    const humanChoice1 = humanChoice.toUpperCase();
+    const computerChoice1 = computerChoice.toUpperCase();
+    if(humanChoice1 === computerChoice1){
+        alert ("Draw! " + "You picked: " + humanChoice1 + " and the computer picked " + computerChoice1)
+    }
+    else if (
+        (humanChoice1 === "rock" && computerChoice1 ==="paper") || 
+        (humanChoice1 === "paper" && computerChoice1 === "scissors" ) ||
+        (humanChoice1 === "scissors" && computerChoice1 === "rock" ) 
+        ){
+        alert ("You lose! " + humanChoice1 + " beats " + computerChoice1);
+        computerScore++;
+    }
+    else {
+        alert ("You won! " + humanChoice1 + " beats " + computerChoice1 );
+        humanScore++;
+    }
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+// console.log(humanScore);
+// console.log(computerScore);
